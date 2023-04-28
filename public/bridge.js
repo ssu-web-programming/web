c528618063a2b093f992d6f7b8fa304c5cae3769
안용덕 <diu413@polarisoffice.com>
2023-04-28 16:46:00 +0900

    getAgentPlatform 테스트 함수 추가

--- public/bridge.js
+++ public/bridge.js
@@ -1,5 +1,23 @@
+function getAgentPlatform(userAgent) {
+  if (userAgent) {
+    let ua = userAgent.toLowerCase();
+
+    if (ua.search('android') > -1) {
+      return 'android';
+    } else if (ua.search('iphone') > -1 || ua.search('ipod') > -1 || ua.search('ipad') > -1) {
+      return 'ios';
+    } else if (ua.search('macintosh') > -1) {
+      return 'mac';
+    } else if (ua.search('win') > -1) {
+      return 'pc';
+    }
+  }
+
+  return 'unkwon';
+}
+
 const getPlatform = () => {
-  return navigator.userAgent;
+  return getAgentPlatform(navigator.userAgent);
 };
 
 window._Bridge = {
