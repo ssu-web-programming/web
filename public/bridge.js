ff67280a3b6c3ed8c7d4d813c65ea01e2dbda8e1
안용덕 <diu413@polarisoffice.com>
2023-05-02 09:59:33 +0900

    Client Platform Define 추가

--- public/bridge.js
+++ public/bridge.js
@@ -1,19 +1,28 @@
+const CLIENT_TYPE = {
+  UNKNOWN: 'unknown',
+  ANDROID: 'android',
+  IOS: 'ios',
+  MAC: 'mac',
+  PC: 'pc',
+  WEB: 'web'
+};
+
 function getAgentPlatform(userAgent) {
   if (userAgent) {
     let ua = userAgent.toLowerCase();
 
     if (ua.search('android') > -1) {
-      return 'android';
+      return CLIENT_TYPE.ANDROID;
     } else if (ua.search('iphone') > -1 || ua.search('ipod') > -1 || ua.search('ipad') > -1) {
-      return 'ios';
+      return CLIENT_TYPE.IOS;
     } else if (ua.search('macintosh') > -1) {
-      return 'mac';
+      return CLIENT_TYPE.MAC;
     } else if (ua.search('win') > -1) {
-      return 'pc';
+      return CLIENT_TYPE.PC;
     }
   }
 
-  return 'unkwon';
+  return CLIENT_TYPE.UNKNOWN;
 }
 
 const getPlatform = () => {
@@ -38,18 +47,18 @@ const getDelegator = (call) => {
   try {
     const platform = getPlatform();
     switch (platform) {
-      case 'android': {
+      case CLIENT_TYPE.ANDROID: {
         return window.Native[call];
       }
-      case 'ios':
-      case 'mac': {
+      case CLIENT_TYPE.IOS:
+      case CLIENT_TYPE.MAC: {
         return window.webkit.messageHandlers[call].postMessage;
       }
-      // case 'pc': {
+      // case CLIENT_TYPE.PC: {
       //   window.chrome.webview.postMessage(msg);
       //   break;
       // }
-      // case 'web': {
+      // case CLIENT_TYPE.WEB: {
       //   break;
       // }
       default: {
