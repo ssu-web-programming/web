b478b512e0f5ea517189feb82ad87968f9fe5fe7
안용덕 <diu413@polarisoffice.com>
2023-05-05 13:08:55 +0900

    build Script 추가

---
+++ env/.env.prod.js
@@ -0,0 +1,12 @@
+module.exports = new Promise((resolve, reject) => {
+  resolve();
+  // Promise.all([common_promise]).then((resultArr) => {
+  //   const common = resultArr[0];
+  //   resolve({
+  //     ...common,
+  //     BACK_END_SERVER_URL,
+  //     REACT_APP_USING_COLLABORATION,
+  //     REACT_APP_GA_UA_TRACKING_ID
+  //   });
+  // });
+});
---
+++ scripts/ready-to-deploy-utils.js
@@ -0,0 +1,77 @@
+import { spawnSync } from 'child_process';
+import fs from 'fs';
+import archiver from 'archiver';
+import xml2js from 'xml2js';
+import * as rra from 'recursive-readdir-async';
+
+export const makeAuditReport = (targetDir, fileName) => {
+  spawnSync(`yarn`, ['genAuditReport', `./${targetDir}/${fileName}`], {
+    stdio: 'inherit',
+    shell: true
+  });
+};
+
+export const zipBuildResult = (targetDir, fileName) => {
+  const output = fs.createWriteStream(`./${targetDir}/${fileName}`);
+  const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+  });
+
+  // good practice to catch warnings (ie stat failures and other non-blocking errors)
+  archive.on('warning', function (err) {
+    if (err.code === 'ENOENT') {
+      // log warning
+    } else {
+      // throw error
+      throw err;
+    }
+  });
+
+  // good practice to catch this error explicitly
+  archive.on('error', function (err) {
+    throw err;
+  });
+
+  archive.pipe(output);
+  archive.directory('./build', false);
+  //archive.file('./ver.json', { date: JSON.stringify({ rev }) });
+
+  archive.finalize();
+};
+
+export const getSVNRev = (workingDir, targetDir) => {
+  return new Promise((resolve, reject) => {
+    //const tempID = Buffer.from(Math.random().toString()).toString('base64');
+
+    spawnSync('svn', ['info', '--xml', '>', `${targetDir}/svnInfo.xml`], {
+      stdio: 'inherit',
+      shell: true,
+      cwd: workingDir
+    });
+
+    const parser = new xml2js.Parser();
+    const svnInfo = fs.readFileSync(`${targetDir}/svnInfo.xml`, 'utf8');
+
+    parser.parseStringPromise(svnInfo).then(function (result) {
+      resolve(result.info.entry[0].commit[0].$.revision);
+    });
+  });
+};
+
+export const listSpecificExtFile = async (folder, ext) => {
+  try {
+    const list = await rra.list(folder, { extensions: true });
+
+    const res = list
+      .filter((file) => {
+        return file.extension === `.${ext}`;
+      })
+      .filter((file) => {
+        return file.isDirectory === false;
+      });
+
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
