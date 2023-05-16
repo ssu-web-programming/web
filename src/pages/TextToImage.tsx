819cf58eb5aed69d3ee634481a6873c97cec4f1d
Bonnie Daeun Hyoung <bonnie.d.hyoung@polarisoffice.com>
2023-05-16 10:31:22 +0900

    SVR-2640 TextToImage Header title 'AI 도구'로 통일 및 수정

--- src/pages/TextToImage.tsx
+++ src/pages/TextToImage.tsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router';
 const TextToImage = () => {
   const location = useLocation();
   return (
-    <HeaderPageLayout title="AI Tools" subTitle="Text to Image">
+    <HeaderPageLayout title="AI 도구" subTitle="Text to Image">
       <ImageCreate key={location.state?.time} contents={location.state?.body} />
     </HeaderPageLayout>
   );
