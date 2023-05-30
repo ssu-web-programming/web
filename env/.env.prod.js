9535c3d50aecdab9c98bcc264fa5f21bcd045924
장석훈 <shjang106@polarisoffice.com>
2023-05-30 17:50:02 +0900

    POAI-55 오타 수정

--- env/.env.prod.js
+++ env/.env.prod.js
@@ -2,7 +2,7 @@ const REACT_APP_USE_LOGGER_SPLUNK = true;
 const REACT_APP_USE_LOGGER_MODE_VF = false;
 
 module.exports = new Promise((resolve, reject) => {
-  resolve({ REACT_APP_USE_LOGGER_SPLUNK, REACT_APP_USE_LOGGER_VF });
+  resolve({ REACT_APP_USE_LOGGER_SPLUNK, REACT_APP_USE_LOGGER_MODE_VF });
   // Promise.all([common_promise]).then((resultArr) => {
   //   const common = resultArr[0];
   //   resolve({
