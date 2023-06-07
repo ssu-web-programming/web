53f50c97d7e5be9db0ea03059290375bc39a7441
Bonnie Daeun Hyoung <bonnie.d.hyoung@polarisoffice.com>
2023-06-07 14:25:36 +0900

    POAI-60 401 AiWrite 작성탭 입력/결과 컴포넌트 분리

--- src/views/AIWriteTab.tsx
+++ src/views/AIWriteTab.tsx
@@ -1,17 +1,5 @@
-import styled, { css } from 'styled-components';
-import SubTitle from '../components/SubTitle';
-import {
-  BoldLengthWrapper,
-  ColumDivider,
-  LengthWrapper,
-  RightBox,
-  RowBox,
-  exampleList
-} from './AIChatTab';
-import Button from '../components/Button';
-import Icon from '../components/Icon';
+import styled from 'styled-components';
 import { useRef, useState } from 'react';
-import OpenAILinkText from '../components/OpenAILinkText';
 import { useDispatch } from 'react-redux';
 import { calcToken } from '../api/usePostSplunkLog';
 import { activeToast } from '../store/slices/toastSlice';
@@ -25,117 +13,18 @@ import {
   updateWriteHistory
 } from '../store/slices/writeHistorySlice';
 import { useAppSelector } from '../store/store';
-import { selectTabSlice } from '../store/slices/tabSlice';
-import ExTextbox from '../components/ExTextbox';
-import IconButton from '../components/IconButton';
 import { formRecList } from '../components/FuncRecBox';
-import icon_write from '../img/ico_creating_text_white.svg';
-import icon_prev from '../img/ico_arrow_prev.svg';
-import icon_next from '../img/ico_arrow_next.svg';
-import PreMarkdown from '../components/PreMarkdown';
-import CopyIcon from '../components/CopyIcon';
-import StopButton from '../components/StopButton';
-import icon_chat_white from '../img/ico_chat_white.svg';
-import {
-  TableCss,
-  flexColumn,
-  flex,
-  alignItemCenter,
-  purpleBtnCss,
-  justiSpaceBetween,
-  justiCenter,
-  flexGrow,
-  flexShrink,
-  grid3Btn
-} from '../style/cssCommon';
-import RecreatingButton from '../components/RecreatingButton';
+import { grid3Btn } from '../style/cssCommon';
 import { useMoveChatTab } from '../components/hooks/useMovePage';
 import { setCreating } from '../store/slices/tabSlice';
-import Loading from '../components/Loading';
 import { JSON_CONTENT_TYPE, CHAT_STREAM_API } from '../api/constant';
-import { calLeftCredit, insertDoc } from '../util/common';
+import { calLeftCredit } from '../util/common';
 import useApiWrapper from '../api/useApiWrapper';
 import { useTranslation } from 'react-i18next';
 import useErrorHandle from '../components/hooks/useErrorHandle';
 import { GPT_EXCEEDED_LIMIT } from '../error/error';
-import NoBorderButton from '../components/NoBorderButton';
-import ShowResult from '../components/ShowResult';
-
-const WriteInputPage = styled.div`
-  ${flexColumn}
-  padding: 16px;
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-
-  gap: 16px;
-`;
-
-const InputArea = styled.div`
-  ${flex}
-
-  width: 100%;
-  /* margin-bottom: 17px; */
-`;
-
-const ResultBox = styled.div`
-  width: 100%;
-  max-height: 70%;
-  flex: 1;
-  border-radius: 4px;
-  background-color: #fff;
-  /* padding: 8px 12px 0px 12px; */
-  box-sizing: border-box;
-  margin-bottom: 8px;
-  height: 620px;
-  ${flexGrow}
-  ${flexShrink}
-  ${flexColumn}
-  ${justiSpaceBetween}
-
-  ${TableCss}
-`;
-
-const LoadingWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  font-size: 13px;
-  font-weight: 500;
-  color: var(--ai-purple-50-main);
-  word-wrap: break-word;
-  div {
-    width: 196px;
-    text-align: center;
-  }
-  ${flexColumn}
-  ${justiCenter}
-  ${alignItemCenter}
-`;
-
-const ResultWrapper = styled.div`
-  ${flex}
-  overflow: auto;
-  white-space: break-spaces;
-  width: 100%;
-  padding: 8px 12px;
-  box-sizing: border-box;
-`;
-
-const ResWrapper = styled.div`
-  ${flexColumn}
-  padding: 16px;
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-  background-color: var(--ai-purple-99-bg-light);
-
-  gap: 8px;
-`;
-
-const TitleInputSet = styled.div`
-  ${flexColumn}
-  gap: 8px;
-`;
+import AiWriteResult from '../components/aiWrite/AiWriteResult';
+import AIWriteInput from '../components/aiWrite/AIWriteInput';
 
 export const Grid3BtnContainer = styled.div`
   ${grid3Btn}
@@ -144,24 +33,22 @@ export const Grid3BtnContainer = styled.div`
   gap: 8px;
 `;
 
-const lengthList = [
-  { title: 'Short', length: 'short' },
-  { title: 'Medium', length: 'medium' },
-  { title: 'Long', length: 'long' }
-];
-
-interface FormListType {
+export interface FormListType {
   id: string;
   icon?: string;
   title: string;
 }
 
-interface LengthListType {
+export interface LengthListType {
   title: string;
   length: string;
 }
 
-const subjectMaxLength = 1000;
+export const lengthList = [
+  { title: 'Short', length: 'short' },
+  { title: 'Medium', length: 'medium' },
+  { title: 'Long', length: 'long' }
+];
 
 const AIWriteTab = () => {
   const apiWrapper = useApiWrapper();
@@ -170,7 +57,6 @@ const AIWriteTab = () => {
   const [selectedLength, setSelectedLength] = useState<LengthListType>(lengthList[0]);
   const { t } = useTranslation();
 
-  const { creating } = useAppSelector(selectTabSlice);
   const errorHandle = useErrorHandle();
 
   const stopRef = useRef<boolean>(false);
@@ -308,290 +194,32 @@ const AIWriteTab = () => {
     }
   };
 
-  const currentWrite = history.filter((write) => write.id === currentWriteId)[0];
-  const currentIndex = history.findIndex((write) => write.id === currentWriteId);
+  const onClickStop = () => {
+    stopRef.current = true;
+  };
+
   return (
     <>
       {!currentWriteId ? (
-        <WriteInputPage>
-          <TitleInputSet>
-            <RowBox
-              cssExt={css`
-                ${justiSpaceBetween}
-              `}>
-              <SubTitle subTitle={t(`WriteTab.WriteTopic`)} />
-              <ShowResult
-                disable={history.length === 0}
-                onClick={() => {
-                  dispatch(setCurrentWrite(history[0].id));
-                }}
-              />
-            </RowBox>
-            <InputArea>
-              <ExTextbox
-                exampleList={exampleList}
-                maxtTextLen={subjectMaxLength}
-                value={subject}
-                placeholder={t(`WriteTab.WriteTextboxPlacehold`) || ''}
-                setValue={(val: string) => {
-                  setSubject(val);
-                }}></ExTextbox>
-            </InputArea>
-          </TitleInputSet>
-
-          <TitleInputSet>
-            <SubTitle subTitle={t(`WriteTab.SelectForm`)} />
-            <Grid3BtnContainer>
-              {formRecList.map((form) => (
-                <div>
-                  <IconButton
-                    iconCssExt={css`
-                      background-color: ${selectedForm.id === form.id
-                        ? 'var(--ai-purple-97-list-over)'
-                        : 'var(--gray-gray-20)'};
-                      box-sizing: border-box;
-                    `}
-                    key={form.id}
-                    title={t(`FormList.${form.title}`)}
-                    onClick={() => {
-                      setSelectedForm(form);
-                    }}
-                    selected={selectedForm ? (selectedForm.id === form.id ? true : false) : false}
-                    icon={selectedForm.id === form.id ? form.selectedIcon : form.icon}
-                  />
-                </div>
-              ))}
-            </Grid3BtnContainer>
-          </TitleInputSet>
-
-          <TitleInputSet>
-            <SubTitle subTitle={t(`WriteTab.SelectResultLength`)} />
-            <Grid3BtnContainer>
-              {lengthList.map((length, index) => (
-                <NoBorderButton
-                  key={index}
-                  onClick={() => {
-                    setSelectedLength(length);
-                  }}
-                  selected={
-                    selectedLength
-                      ? selectedLength.length === length.length
-                        ? true
-                        : false
-                      : false
-                  }
-                  cssExt={css`
-                    border: none;
-                    background-color: ${selectedLength && selectedLength.length === length.length
-                      ? `var(--ai-purple-97-list-over)`
-                      : 'var(--gray-gray-20)'};
-                    flex: none;
-                    font-size: 13px;
-                    font-weight: ${selectedLength?.length === length.length ? 'border' : 'none'};
-                    font-stretch: normal;
-                    font-style: normal;
-                    line-height: 1.54;
-                    letter-spacing: normal;
-
-                    box-sizing: border-box;
-
-                    width: 100%;
-                    padding: 4px 0px;
-                    ${flexShrink}
-                    ${flexGrow}
-                  `}>
-                  {t(`WriteTab.Length.${length.title}`)}
-                </NoBorderButton>
-              ))}
-            </Grid3BtnContainer>
-          </TitleInputSet>
-
-          <div>
-            <Button
-              disable={subject.length === 0}
-              isCredit={true}
-              cssExt={css`
-                ${purpleBtnCss}
-                width: 100%;
-                margin-top: 4px;
-              `}
-              onClick={() => {
-                if (subject.length === 0) {
-                  return;
-                }
-
-                submitSubject();
-              }}
-              icon={icon_write}>
-              {t(`WriteTab.WritingArticle`)}
-            </Button>
-          </div>
-        </WriteInputPage>
+        <AIWriteInput
+          history={history}
+          subject={subject}
+          setSubject={setSubject}
+          selectedForm={selectedForm}
+          setSelectedForm={setSelectedForm}
+          setSelectedLength={setSelectedLength}
+          selectedLength={selectedLength}
+          submitSubject={submitSubject}
+        />
       ) : (
-        <ResWrapper>
-          <RowBox>
-            <SubTitle subTitle={t(`WriteTab.PreviewWriting`)} />
-            {creating === 'none' && (
-              <RecreatingButton
-                onClick={() => {
-                  // dispatch(initWriteHistory()); // 같은 주제끼리만 저장할지 의논 필요
-                  // initAllInput();
-                  dispatch(resetCurrentWrite());
-                }}
-              />
-            )}
-          </RowBox>
-          <ResultBox>
-            {currentWrite.result.length === 0 && creating === 'Write' ? (
-              <LoadingWrapper>
-                <Loading>{t(`WriteTab.LoadingMsg`)}</Loading>
-              </LoadingWrapper>
-            ) : (
-              <ResultWrapper>
-                <PreMarkdown text={currentWrite.result} endRef={endRef} />
-              </ResultWrapper>
-            )}
-            <div>
-              {currentWrite.result.length > 0 && creating === 'Write' && (
-                <StopButton
-                  cssExt={css`
-                    margin: 0 auto;
-                    margin-bottom: 16px;
-                    margin-top: 16px;
-                  `}
-                  onClick={() => {
-                    stopRef.current = true;
-                  }}
-                />
-              )}
-              {(creating === 'none' || currentWrite.result.length > 0) && <ColumDivider />}
-              <RowBox
-                cssExt={css`
-                  ${alignItemCenter}
-                  color: var(--gray-gray-70);
-                  font-size: 13px;
-                  height: 35px;
-
-                  padding: 8px 12px;
-                  box-sizing: border-box;
-                `}>
-                <BoldLengthWrapper>
-                  {currentWrite.result.length > 0 && (
-                    <>{t(`WriteTab.LengthInfo`, { length: currentWrite?.result.length })}</>
-                  )}
-                </BoldLengthWrapper>
-
-                {creating === 'none' && (
-                  <RightBox>
-                    <Icon
-                      cssExt={css`
-                        width: 16px;
-                        height: 16px;
-                        margin-right: 11px;
-                        opacity: ${currentIndex === 0 && '0.3'};
-                      `}
-                      iconSrc={icon_prev}
-                      onClick={() => {
-                        if (currentIndex > 0) {
-                          dispatch(setCurrentWrite(history[currentIndex - 1]?.id));
-                        }
-                      }}
-                    />
-                    <div>
-                      {history.findIndex((write) => write.id === currentWriteId) + 1}/
-                      {history.length}
-                    </div>
-                    <Icon
-                      cssExt={css`
-                        width: 16px;
-                        height: 16px;
-                        margin-left: 11px;
-                        opacity: ${currentIndex === history.length - 1 && '0.3'};
-                      `}
-                      iconSrc={icon_next}
-                      onClick={() => {
-                        if (currentIndex < history.length - 1) {
-                          dispatch(setCurrentWrite(history[currentIndex + 1]?.id));
-                        }
-                      }}
-                    />
-                    {/* <CopyIcon
-                          cssExt={css`
-                            margin-left: 12px;
-                          `}
-                          onClick={() => {
-                            // TODO: 복사 로직
-
-                            dispatch(
-                              activeToast({
-                                active: true,
-                                msg: t(`ToastMsg.CompleteCopy`),
-                                isError: false
-                              })
-                            );
-                          }}
-                        /> */}
-                  </RightBox>
-                )}
-              </RowBox>
-            </div>
-          </ResultBox>
-          {creating === 'none' && (
-            <>
-              <RowBox
-                cssExt={css`
-                  gap: 8px;
-                `}>
-                <Button
-                  cssExt={css`
-                    margin: 0px;
-                  `}
-                  isCredit={true}
-                  onClick={() => {
-                    submitSubject(currentWrite);
-                  }}>
-                  {t(`WriteTab.Recreating`)}
-                </Button>
-                <Button
-                  cssExt={css`
-                    margin: 0px;
-                  `}
-                  onClick={() => {
-                    insertDoc(currentWrite.result);
-
-                    dispatch(
-                      activeToast({
-                        active: true,
-                        msg: t(`ToastMsg.CompleteInsert`),
-                        isError: false
-                      })
-                    );
-                  }}>
-                  {t(`WriteTab.InsertDoc`)}
-                </Button>
-              </RowBox>
-              <div>
-                <Button
-                  icon={icon_chat_white}
-                  cssExt={css`
-                    flex: none;
-                    width: 100%;
-                    box-sizing: border-box;
-                    margin: 0;
-                    ${purpleBtnCss}
-                  `}
-                  onClick={() => {
-                    moveChat(currentWrite.result);
-                  }}>
-                  {t(`WriteTab.MoveToChating`)}
-                </Button>
-              </div>
-              <RightBox>
-                <OpenAILinkText />
-              </RightBox>
-            </>
-          )}
-        </ResWrapper>
+        <AiWriteResult
+          history={history}
+          endRef={endRef}
+          onClickStop={onClickStop}
+          currentWriteId={currentWriteId}
+          submitSubject={submitSubject}
+          moveChat={moveChat}
+        />
       )}
     </>
   );
