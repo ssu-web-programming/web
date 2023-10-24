b42afcddf35d0825adac82b6e5f10a1fbbe57dd2
shjang106 <shjang106@polarisoffice.com>
2023-10-24 15:41:30 +0900

    POAI-148 get-voice BE path 변경

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -7,7 +7,7 @@ export const BANNER_ACTIVE_API = '/api/v2/shareTech/getEnableStatus';
 export const ASKDOC_API = '/api/v2/askdoc/chat';
 export const ASKDOC_INIT_QUESTION_API = '/api/v2/askdoc/make-question';
 
-export const VOICEDOC_MAKE_VOICE = '/api/v2/askdoc/get-voice';
+export const VOICEDOC_MAKE_VOICE = '/api/v2/tts/get-voice';
 
 export const JSON_CONTENT_TYPE = {
   'content-type': 'application/json'
