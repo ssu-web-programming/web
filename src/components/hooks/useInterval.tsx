830c95dd1f7bb694b98d8b193a5a9819e8aae9f4
frankie <frankie.j.kim@polarisoffice.com>
2023-12-26 16:10:35 +0900

    CGP-224 Polling 처리를 위한 interval hook 추가

---
+++ src/components/hooks/useInterval.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+interface IUseInterval {
+  (callback: () => void, interval: number | null): void;
+}
+
+const useInterval: IUseInterval = (callback, interval) => {
+  const savedCallback = useRef<(() => void) | null>(null);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+
+    if (interval !== null) {
+      let id = setInterval(tick, interval);
+      return () => clearInterval(id);
+    }
+  }, [interval]);
+};
+
+export default useInterval;
