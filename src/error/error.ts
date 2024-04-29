cb00d65839816dd9f1f73c862693abf4f4de1a0c
shjang106 <shjang106@polarisoffice.com>
2024-04-29 09:40:04 +0900

    WGB-2609 크레딧 없는 경우 채팅창에 출력되는 문제 수정

--- src/error/error.ts
+++ src/error/error.ts
@@ -2,3 +2,16 @@ export const ERR_NOT_ONLINE = 'ERR_NOT_ONLINE';
 export const ERR_INVALID_SESSION = 'ERR_INVALID_SESSION';
 export const INVALID_PROMPT = 'INVALID_PROMPT';
 export const GPT_EXCEEDED_LIMIT = 'GPT_EXCEEDED_LIMIT';
+const NOT_ENOUGH_CREDIT = 'NOT_ENOUGH_CREDIT';
+
+interface CreditStatus {
+  current: number;
+  necessary: number;
+}
+export class NoCreditError extends Error {
+  credit: CreditStatus;
+  constructor(credit: CreditStatus) {
+    super(NOT_ENOUGH_CREDIT);
+    this.credit = credit;
+  }
+}
