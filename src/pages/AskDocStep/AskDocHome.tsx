3223c453fb3e915611586514ad2b1b01ef00b169
shjang106 <shjang106@polarisoffice.com>
2024-08-19 14:07:52 +0900

    CGP-509 ask doc EOS

--- src/pages/AskDocStep/AskDocHome.tsx
+++ src/pages/AskDocStep/AskDocHome.tsx
@@ -3,18 +3,31 @@ import Header from '../../components/layout/Header';
 import { useTranslation } from 'react-i18next';
 import { WrapperPage, Body } from '../../style/askDoc';
 import Modals, { Overlay } from '../../components/askDoc/modals/Modals';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import useGtmPageChange from '../../components/hooks/useGtmPageChange';
+import { useConfirm } from 'components/Confirm';
+import Bridge from 'util/bridge';
 
 export const AskDocHome = () => {
   const { t } = useTranslation();
+  const confirm = useConfirm();
   useGtmPageChange();
+
+  useEffect(() => {
+    confirm({
+      title: t('EOS.ASKDocEOSTitle2')!,
+      msg: t('EOS.ASKDocEOSDesc2')!,
+      onOk: {
+        text: t('Confirm'),
+        callback: () => Bridge.callBridgeApi('closePanel', '')
+      }
+    });
+  }, []);
+
   return (
     <WrapperPage>
       <Header title={t('AITools')} subTitle={'ASK Doc'}></Header>
-      <Body>
-        <Outlet />
-      </Body>
+      <Body>{/* <Outlet /> */}</Body>
       <Suspense fallback={<Overlay />}>
         <Modals />
       </Suspense>
