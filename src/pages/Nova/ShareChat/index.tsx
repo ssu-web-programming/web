80c1f2b0201989c13552116c3d91122a7da5d73b
luna <luna.s.jo@polarisoffice.com>
2024-11-29 11:43:29 +0900

    aiChat 대화 공유하기 기능 UI 구현 완료

---
+++ src/pages/Nova/ShareChat/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { ReactComponent as IconLogoNova } from 'img/ico_logo_nova.svg';
+import { marked } from 'marked';
+import { isMobile } from 'util/bridge';
+
+import Icon from '../../../components/Icon';
+import { getFileIcon } from '../../../components/nova/InputBar';
+import ico_ai from '../../../img/ico_ai.svg';
+import ico_user from '../../../img/ico_user.svg';
+
+import data from './chat_history.json';
+import * as S from './style';
+import { DateWithGuide } from './style';
+
+export default function ShareChat() {
+  return (
+    <S.Wrapper $isMobile={isMobile}>
+      <S.Header>
+        <IconLogoNova width={107} height={32} />
+        <DateWithGuide $isMobile={isMobile}>
+          <span className="date">2024년 11월 29일</span>
+          <span className="guide">※ 본 링크는 7일 간만 유효합니다.</span>
+        </DateWithGuide>
+      </S.Header>
+      <S.Content>
+        {data.map((item, index) => (
+          <S.Message key={index}>
+            <p>
+              <strong>
+                {item.type === 'question' ? (
+                  <Icon size={32} iconSrc={ico_user}></Icon>
+                ) : (
+                  <Icon size={32} iconSrc={ico_ai}></Icon>
+                )}
+              </strong>
+            </p>
+
+            <S.Detail>
+              <div dangerouslySetInnerHTML={{ __html: marked(item.content) }} />
+              {item.files.map((file, idx) => (
+                <S.FileItem key={idx}>
+                  <Icon size={28} iconSrc={getFileIcon(file)} />
+                  {file}
+                </S.FileItem>
+              ))}
+            </S.Detail>
+          </S.Message>
+        ))}
+      </S.Content>
+    </S.Wrapper>
+  );
+}
---
+++ src/pages/Nova/ShareChat/style.ts
@@ -0,0 +1,123 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div<{
+  $isMobile: boolean;
+}>`
+  width: 100%;
+  padding: ${({ $isMobile }) => ($isMobile ? '24px 16px' : '80px 200px')};
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 20px;
+`;
+
+export const DateWithGuide = styled.div<{
+  $isMobile: boolean;
+}>`
+  width: 100%;
+  display: flex;
+  flex-direction: ${({ $isMobile }) => ($isMobile ? 'column' : 'row')};
+  align-items: ${({ $isMobile }) => ($isMobile ? 'flex-start' : 'center')};
+  justify-content: space-between;
+
+  .date {
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 30px;
+    color: #26282b;
+  }
+
+  .guide {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 21px;
+    color: #454c53;
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const Message = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+
+  img {
+    max-width: 160px;
+    align-self: flex-start;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-radius: 6px;
+    width: 100%;
+  }
+
+  th,
+  td {
+    padding: 1em;
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+  }
+
+  table,
+  tr,
+  td,
+  th {
+    border-radius: 6px;
+    border: 1px solid #e0d1ff;
+    padding: 15px 10px;
+  }
+
+  th {
+    border: 1px solid #e0d1ff;
+    color: #6f3ad0;
+    background: #f5f1fd;
+    padding: 10px 10px;
+  }
+`;
+
+export const Detail = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const FileItem = styled.div`
+  width: fit-content;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  margin-top: 10px;
+  padding: 6px;
+  border: 1px solid var(--gray-gray-40);
+  border-radius: 8px;
+  background: var(--gray-gray-20);
+
+  font-size: 14px;
+  line-height: 21px;
+  text-align: left;
+  color: var(--gray-gray-90-01);
+`;
+
+export const Logo = styled.img`
+  width: 22px;
+  height: 28px;
+`;
