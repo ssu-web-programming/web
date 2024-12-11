4fe9de58b3c9065e241b3287419da4d261bdcf9e
harvey.h.kim <harvey.h.kim@polarisoffice.com>
2024-12-11 09:14:08 +0900

    feat: slide speed 수정 작업 완료

--- src/components/nova/tabs/index.tsx
+++ src/components/nova/tabs/index.tsx
@@ -1,31 +1,31 @@
 import { useEffect, useRef, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import styled from 'styled-components';
+import { NOVA_TAB_TYPE } from 'store/slices/tabSlice';
+import { FreeMode } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Swiper as SwiperClass } from 'swiper/types';
+import { isMobile } from 'util/bridge';
 
-import { ReactComponent as IconConvert } from '../../img/nova/tab/convert_Img.svg';
-import convertIcon from '../../img/nova/tab/convert_Img.svg';
-import convert2DTo3DIcon from '../../img/nova/tab/tab_3d_n.svg';
-import convert2DTo3DSelectedIcon from '../../img/nova/tab/tab_3d_s.svg';
-import aiChatIcon from '../../img/nova/tab/tab_ai_chat_n.svg';
-import aiChatSelectedIcon from '../../img/nova/tab/tab_ai_chat_s.svg';
-import leftArrowIcon from '../../img/nova/tab/tab_arrow_left.svg';
-import rightArrowIcon from '../../img/nova/tab/tab_arrow_right.svg';
-import changeBGIcon from '../../img/nova/tab/tab_bg_change_n.svg';
-import changeBGSelectedIcon from '../../img/nova/tab/tab_bg_change_s.svg';
-import removeBGIcon from '../../img/nova/tab/tab_bg_delete_n.svg';
-import removeBGSelectedIcon from '../../img/nova/tab/tab_bg_delete_s.svg';
-import expandImgIcon from '../../img/nova/tab/tab_expansion_n.svg';
-import expandImgSelectedIcon from '../../img/nova/tab/tab_expansion_s.svg';
-import improvedResIcon from '../../img/nova/tab/tab_quality_n.svg';
-import improvedResSelectedIcon from '../../img/nova/tab/tab_quality_s.svg';
-import remakeImgIcon from '../../img/nova/tab/tab_remake_n.svg';
-import remakeImgSelectedIcon from '../../img/nova/tab/tab_remake_s.svg';
-import changeStyleIcon from '../../img/nova/tab/tab_style_n.svg';
-import changeStyleSelectedIcon from '../../img/nova/tab/tab_style_s.svg';
-import { NOVA_TAB_TYPE } from '../../store/slices/tabSlice';
-import { isMobile } from '../../util/bridge';
+import convert2DTo3DIcon from '../../../img/nova/tab/tab_3d_n.svg';
+import convert2DTo3DSelectedIcon from '../../../img/nova/tab/tab_3d_s.svg';
+import aiChatIcon from '../../../img/nova/tab/tab_ai_chat_n.svg';
+import aiChatSelectedIcon from '../../../img/nova/tab/tab_ai_chat_s.svg';
+import leftArrowIcon from '../../../img/nova/tab/tab_arrow_left.svg';
+import rightArrowIcon from '../../../img/nova/tab/tab_arrow_right.svg';
+import changeBGIcon from '../../../img/nova/tab/tab_bg_change_n.svg';
+import changeBGSelectedIcon from '../../../img/nova/tab/tab_bg_change_s.svg';
+import removeBGIcon from '../../../img/nova/tab/tab_bg_delete_n.svg';
+import removeBGSelectedIcon from '../../../img/nova/tab/tab_bg_delete_s.svg';
+import expandImgIcon from '../../../img/nova/tab/tab_expansion_n.svg';
+import expandImgSelectedIcon from '../../../img/nova/tab/tab_expansion_s.svg';
+import improvedResIcon from '../../../img/nova/tab/tab_quality_n.svg';
+import improvedResSelectedIcon from '../../../img/nova/tab/tab_quality_s.svg';
+import remakeImgIcon from '../../../img/nova/tab/tab_remake_n.svg';
+import remakeImgSelectedIcon from '../../../img/nova/tab/tab_remake_s.svg';
+import changeStyleIcon from '../../../img/nova/tab/tab_style_n.svg';
+import changeStyleSelectedIcon from '../../../img/nova/tab/tab_style_s.svg';
+
+import * as S from './style';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -42,83 +42,6 @@ const iconMap: Record<NOVA_TAB_TYPE, { default: string; selected: string }> = {
   improvedRes: { default: improvedResIcon, selected: improvedResSelectedIcon },
   changeStyle: { default: changeStyleIcon, selected: changeStyleSelectedIcon }
 };
-
-const Wrap = styled.div`
-  width: 100%;
-  height: 52px;
-  min-height: 52px;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 8px;
-  padding: 8px 16px;
-  z-index: 1;
-`;
-
-const Tap = styled.div<{ isHighlighted: boolean }>`
-  height: 32px;
-  display: flex;
-  align-items: center;
-  padding: 6px 12px 6px 8px;
-  border: 1px solid ${(props) => (props.isHighlighted ? '#c6a9ff' : '#c9cdd2')};
-  border-radius: 8px;
-  background-color: ${(props) => (props.isHighlighted ? '#ede5fe' : 'white')};
-  cursor: pointer;
-`;
-
-const Badge = styled.div`
-  width: 16px;
-  height: 16px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #fb4949;
-  border-radius: 4px;
-
-  span {
-    font-size: 10px;
-    font-weight: bold;
-    line-height: 10px;
-    color: white;
-  }
-`;
-
-const Text = styled.div<{ isHighlighted: boolean }>`
-  font-size: 14px;
-  font-weight: ${(props) => (props.isHighlighted ? 500 : 400)};
-  color: ${(props) => (props.isHighlighted ? '#511bb2' : 'black')};
-`;
-
-const CustomNavButton = styled.button<{ isVisible: boolean }>`
-  width: 52px;
-  height: 52px;
-  top: var(--swiper-navigation-top-offset, 42%);
-  display: ${(props) => (props.isVisible ? 'block' : 'none')};
-  padding: 0;
-  background: none;
-  border: none;
-  cursor: pointer;
-  -webkit-tap-highlight-color: transparent;
-
-  &.left {
-    left: 0;
-  }
-
-  &.right {
-    right: 0;
-  }
-
-  :after {
-    display: none;
-  }
-`;
-
-const StyledIconConvert = styled(IconConvert)<{ $isHighlighted: boolean }>`
-  path {
-    fill: ${(props) => (props.$isHighlighted ? '#511bb2' : 'black')};
-  }
-`;
 
 interface TabProps {
   tabs: NOVA_TAB_TYPE[];
@@ -139,7 +62,7 @@ const Tabs = ({ tabs, activeTab, onChangeTab }: TabProps) => {
         <Trans
           i18nKey={`Nova.Tabs.${tab}`}
           components={{
-            img: <StyledIconConvert $isHighlighted={isActive} height={11} />
+            img: <S.StyledIconConvert $isHighlighted={isActive} height={11} />
           }}
         />
       );
@@ -237,8 +160,10 @@ const Tabs = ({ tabs, activeTab, onChangeTab }: TabProps) => {
   };
 
   return (
-    <Wrap>
+    <S.Wrap>
       <Swiper
+        freeMode
+        modules={[FreeMode]}
         spaceBetween={8}
         slidesPerView="auto"
         centeredSlides={isCentered}
@@ -260,15 +185,15 @@ const Tabs = ({ tabs, activeTab, onChangeTab }: TabProps) => {
 
           return (
             <SwiperSlide key={tab} style={{ width: 'auto' }}>
-              <Tap onClick={() => handleMoveToActiveIndex(tab, idx)} isHighlighted={isActive}>
+              <S.Tap onClick={() => handleMoveToActiveIndex(tab, idx)} isHighlighted={isActive}>
                 {tab === NOVA_TAB_TYPE.convert2DTo3D && (
-                  <Badge>
+                  <S.Badge>
                     <span>N</span>
-                  </Badge>
+                  </S.Badge>
                 )}
                 <img src={getIcon(tab, isActive)} alt="logo" />
-                <Text isHighlighted={isActive}>{getTabTranslationKey(tab, isActive)}</Text>
-              </Tap>
+                <S.Text isHighlighted={isActive}>{getTabTranslationKey(tab, isActive)}</S.Text>
+              </S.Tap>
             </SwiperSlide>
           );
         })}
@@ -276,21 +201,21 @@ const Tabs = ({ tabs, activeTab, onChangeTab }: TabProps) => {
 
       {!isMobile && (
         <>
-          <CustomNavButton
+          <S.CustomNavButton
             className="swiper-button-prev left"
             isVisible={!isBeginning}
             onClick={handlePrevClick}>
             <img src={leftArrowIcon} alt="Left Arrow" />
-          </CustomNavButton>
-          <CustomNavButton
+          </S.CustomNavButton>
+          <S.CustomNavButton
             className="swiper-button-next right"
             isVisible={!isEnd}
             onClick={handleNextClick}>
             <img src={rightArrowIcon} alt="Right Arrow" />
-          </CustomNavButton>
+          </S.CustomNavButton>
         </>
       )}
-    </Wrap>
+    </S.Wrap>
   );
 };
 
