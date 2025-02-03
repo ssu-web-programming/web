20cf8025a786e2c653440e84ef64ce14f59693d6
harvey.h.kim <harvey.h.kim@polarisoffice.com>
2025-02-03 17:22:27 +0900

    feat : overlayModal component 작업 완료

---
+++ src/components/overlay-modal/index.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import styled from 'styled-components';
+
+interface OverlayModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  width?: string;
+  height?: string;
+}
+
+interface ModalContentProps {
+  width: string;
+  height: string;
+}
+
+const OverlayModal: React.FC<OverlayModalProps> = ({
+  isOpen,
+  onClose,
+  children,
+  width = '500px',
+  height = 'auto'
+}) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    }
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
+  return createPortal(
+    <ModalWrapper onClick={onClose}>
+      <ModalContent onClick={(e) => e.stopPropagation()} width={width} height={height}>
+        {children}
+      </ModalContent>
+    </ModalWrapper>,
+    document.body
+  );
+};
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const ModalContent = styled.div<ModalContentProps>`
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  max-width: 90%;
+  max-height: 90vh;
+  overflow-y: auto;
+`;
+
+export default OverlayModal;
--- src/pages/Nova/VoiceDictation/components/voice-save-bottom-sheet/index.tsx
+++ src/pages/Nova/VoiceDictation/components/voice-save-bottom-sheet/index.tsx
@@ -1,6 +1,8 @@
 import ModalSheet from 'components/modalSheet';
+import OverlayModal from 'components/overlay-modal';
 import { ReactComponent as File } from 'img/light/nova/voiceDictation/file.svg';
 import { ReactComponent as Mic } from 'img/light/nova/voiceDictation/mic.svg';
+import { overlay } from 'overlay-kit';
 
 import * as S from './style';
 
@@ -10,12 +12,23 @@ interface Props {
 }
 
 export default function VoiceSaveBottomSheet({ isOpened, setIsOpened }: Props) {
+  const handleOpenSaveFileFormat = () => {
+    console.log('123123123123');
+
+    overlay.open(({ isOpen, close }) => {
+      return (
+        <OverlayModal isOpen={isOpen} onClose={close}>
+          <div>안녕!</div>
+        </OverlayModal>
+      );
+    });
+  };
   return (
     <ModalSheet isOpen={isOpened} setIsOpen={setIsOpened}>
       <S.Container>
         <S.Title>저장하기</S.Title>
         <S.ItemWrapper>
-          <S.Item>
+          <S.Item onClick={handleOpenSaveFileFormat}>
             <Mic />
             <p>음성파일</p>
           </S.Item>
