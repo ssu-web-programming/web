26934ef9da045e03ab3f31f4909763abcc7f0d89
harvey.h.kim <harvey.h.kim@polarisoffice.com>
2025-02-18 11:41:40 +0900

    fix: curTab 받아쓰기에서 녹음으로 변경 작업 완료

--- src/pages/Nova/Translation/components/translation-intro/index.tsx
+++ src/pages/Nova/Translation/components/translation-intro/index.tsx
@@ -107,7 +107,7 @@ export default function TranslationIntro() {
               <TranslationFileUploader
                 guideMsg={t('Nova.Translate.UploadGuide')}
                 creditCount={20}
-                curTab={NOVA_TAB_TYPE.voiceDictation}
+                curTab={NOVA_TAB_TYPE.translation}
               />
             </DragAndDrop>
           </S.FileUploaderWrapper>
