a1bd819fa813988b388bf1bbcd1cb320efbc65a8
harvey.h.kim <harvey.h.kim@polarisoffice.com>
2025-02-19 20:40:55 +0900

    fix: 번역일때 openTab currentFile 초기화

--- src/pages/Nova/Translation/components/translation-file-uploader/index.tsx
+++ src/pages/Nova/Translation/components/translation-file-uploader/index.tsx
@@ -128,8 +128,7 @@ export default function TranslationFileUploader({
     if (localFiles.length > 0) return localFiles[0];
     if (driveFiles.length > 0) return driveFiles[0];
 
-    const isNotExistCurrentFile =
-      !currentFile.id || currentFile.type === 'drive' || currentFile.type === 'notSupported';
+    const isNotExistCurrentFile = !currentFile.id;
 
     return isNotExistCurrentFile ? null : currentFile;
   };
