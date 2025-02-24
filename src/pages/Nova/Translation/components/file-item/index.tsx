fe271c972fbf6ddc96ee51dd5d25b4c3c7087beb
harvey.h.kim <harvey.h.kim@polarisoffice.com>
2025-02-25 01:50:08 +0900

    feat:  파일번역 클리어 함수 추가 작업

--- src/pages/Nova/Translation/components/file-item/index.tsx
+++ src/pages/Nova/Translation/components/file-item/index.tsx
@@ -1,5 +1,7 @@
 import Icon from 'components/Icon';
 import { getFileIcon } from 'components/nova/inputBar';
+import { setDriveFiles } from 'store/slices/uploadFiles';
+import { useAppDispatch } from 'store/store';
 
 import * as S from './style';
 
@@ -8,8 +10,14 @@ interface Props {
 }
 
 export default function FileItem({ fileName }: Props) {
+  const dispatch = useAppDispatch();
   return (
     <S.Wrapper>
+      <S.DeleteIcon
+        onClick={() => {
+          dispatch(setDriveFiles([]));
+        }}
+      />
       <Icon size={45} iconSrc={getFileIcon(fileName as string)} />
       <S.FileName>{fileName || ''}</S.FileName>
     </S.Wrapper>
