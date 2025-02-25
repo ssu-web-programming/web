9617a600d88a5d40bf2c59ce65aa2888e1f68755
luna <luna.s.jo@polarisoffice.com>
2025-02-25 16:29:18 +0900

    #CGP-519 Heygen 로고와 버튼이 겹치는 문제 수정

--- src/components/nova/aiVideo/index.tsx
+++ src/components/nova/aiVideo/index.tsx
@@ -63,6 +63,8 @@ export default function AIVideo() {
           <AvatarCard isHideColorPicker={true} />
         </Result>
       );
+    } else if (status === 'loading') {
+      return <Loading />;
     } else {
       return (
         <>
