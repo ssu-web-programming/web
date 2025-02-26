245b53234e86eb2b05785b6e524dfd91c9726a4f
luna <luna.s.jo@polarisoffice.com>
2025-02-26 14:28:56 +0900

    #WGB-3478 [QA][NOVA] NOVA창 > 문서업로드 > 현재장치 > 미지원문서 첨부시, 팝업문구 : 파일형식이 이미지로 표시됨

--- src/components/hooks/nova/useManageFile.tsx
+++ src/components/hooks/nova/useManageFile.tsx
@@ -205,7 +205,7 @@ export function useManageFile({ onFinishCallback, onClearPastedImages }: Props =
       await confirm({
         title: '',
         msg:
-          selectedNovaTab === 'aiChat'
+          selectedNovaTab === 'aiChat' || selectedNovaTab === 'home'
             ? t('Nova.Alert.CommonUnsupportFile')
             : selectedNovaTab === 'voiceDictation'
               ? t('Nova.translation.Alert.CommonUnsupportVoiceFile')
