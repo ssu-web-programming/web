2c7826cfbeaea81f1684fe535d39b2c180b750ce
harvey.h.kim <harvey.h.kim@polarisoffice.com>
2025-02-26 14:52:29 +0900

    chore : debug용 콘솔

--- src/pages/Nova/VoiceDictation/provider/audio-recorder-provider/index.tsx
+++ src/pages/Nova/VoiceDictation/provider/audio-recorder-provider/index.tsx
@@ -277,8 +277,10 @@ export const AudioRecorderProvider: React.FC<AudioRecorderProviderProps> = ({ ch
           mediaRecorderRef.current = null;
 
           if (streamRef.current) {
+            console.log('stream 정리 전', streamRef.current);
             streamRef.current.getTracks().forEach((track) => track.stop());
             streamRef.current = null;
+            console.log('stream 정리 후', streamRef.current);
           }
           if (animationFrameRef.current) {
             cancelAnimationFrame(animationFrameRef.current);
