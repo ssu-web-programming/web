b2425f518d3629ab8c7c8e2f0d9eb464d66bae54
luna <luna.s.jo@polarisoffice.com>
2025-02-27 15:14:49 +0900

    #CGP-591 Nova home 배너 교체

--- src/components/nova/banner/index.tsx
+++ src/components/nova/banner/index.tsx
@@ -1,13 +1,22 @@
 import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import BannerImg from '../../../img/common/banner_test01.png';
+import BannerEn from '../../../img/common/nova/banner/banner_nova_en.png';
+import BannerJa from '../../../img/common/nova/banner/banner_nova_jp.png';
+import BannerKo from '../../../img/common/nova/banner/banner_nova_ko.png';
+import { lang } from '../../../locale';
 
 import * as S from './style';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+
+const bannerImages = {
+  ko: BannerKo,
+  en: BannerEn,
+  ja: BannerJa
+};
 
 const Banner = () => {
   return (
@@ -19,16 +28,7 @@ const Banner = () => {
         navigation={true}
         modules={[Pagination, Navigation]}>
         <SwiperSlide className="swiper-slide">
-          <img src={BannerImg} alt="banner" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <img src={BannerImg} alt="banner" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <img src={BannerImg} alt="banner" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <img src={BannerImg} alt="banner" />
+          <img src={bannerImages[lang] || BannerEn} alt="banner" />
         </SwiperSlide>
       </Swiper>
     </S.SwiperWrapper>
