0328730a9733df0cf3f534545b92e3c8c8513fd2
luna <luna.s.jo@polarisoffice.com>
2025-03-12 16:12:08 +0900

    feat: NOVA 신학기 배너 추가 (CGP-615)

--- src/components/nova/banner/index.tsx
+++ src/components/nova/banner/index.tsx
@@ -1,10 +1,14 @@
 import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import BannerEn from '../../../img/common/nova/banner/banner_nova_en.png';
-import BannerJa from '../../../img/common/nova/banner/banner_nova_jp.png';
-import BannerKo from '../../../img/common/nova/banner/banner_nova_ko.png';
+import BannerEn1 from '../../../img/common/nova/banner/banner1_nova_en.png';
+import BannerJa1 from '../../../img/common/nova/banner/banner1_nova_jp.png';
+import BannerKo1 from '../../../img/common/nova/banner/banner1_nova_ko.png';
+import BannerEn2 from '../../../img/common/nova/banner/banner2_nova_en.png';
+import BannerJa2 from '../../../img/common/nova/banner/banner2_nova_jp.png';
+import BannerKo2 from '../../../img/common/nova/banner/banner2_nova_ko.png';
 import { lang } from '../../../locale';
+import Bridge from '../../../util/bridge';
 
 import * as S from './style';
 
@@ -12,13 +16,30 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-const bannerImages = {
-  ko: BannerKo,
-  en: BannerEn,
-  ja: BannerJa
-};
+const bannerImages = [
+  {
+    image: {
+      ko: BannerKo1,
+      en: BannerEn1,
+      ja: BannerJa1
+    },
+    url: `https://vf-ca-cloud.polarisoffice.com/${lang}/ai`
+  },
+  {
+    image: {
+      ko: BannerKo2,
+      en: BannerEn2,
+      ja: BannerJa2
+    }
+  }
+];
 
 const Banner = () => {
+  const handleOpenPage = (url: string) => {
+    if (url === '') return;
+    Bridge.callBridgeApi('openWindow', url);
+  };
+
   return (
     <S.SwiperWrapper>
       <Swiper
@@ -27,9 +48,14 @@ const Banner = () => {
         }}
         navigation={true}
         modules={[Pagination, Navigation]}>
-        <SwiperSlide className="swiper-slide">
-          <img src={bannerImages[lang] || BannerEn} alt="banner" />
-        </SwiperSlide>
+        {bannerImages.map((banner, index) => (
+          <SwiperSlide
+            className="swiper-slide"
+            key={index}
+            onClick={() => handleOpenPage(banner.url ?? '')}>
+            <img src={banner.image[lang] || BannerEn2} alt="banner" />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </S.SwiperWrapper>
   );
