64035940f6b8f4d8ea8fec417ef4001115c45681
harvey.h.kim <harvey.h.kim@polarisoffice.com>
2025-03-19 18:11:17 +0900

    fix: [QA][NOVA][사용성] 번역 > 텍스트 필드 내 창조절 버튼 고정되지 않음 ( svr-5727 )

--- src/pages/Nova/Translation/components/translation-text-result/index.tsx
+++ src/pages/Nova/Translation/components/translation-text-result/index.tsx
@@ -1,19 +1,6 @@
-import { useEffect } from 'react';
-import { overlay } from 'overlay-kit';
 import { useTranslation } from 'react-i18next'; // 추가
 import { getLangFromLangCode } from 'util/translation';
 
-import { apiWrapper } from '../../../../../api/apiWrapper';
-import { NOVA_GET_CREDIT_USE_COUNT } from '../../../../../api/constant';
-import useErrorHandle from '../../../../../components/hooks/useErrorHandle';
-import SurveyModalContent from '../../../../../components/nova/satisfactionSurvey/survey-modal-content';
-import OverlayModal from '../../../../../components/overlay-modal';
-import { NOVA_TAB_TYPE } from '../../../../../constants/novaTapTypes';
-import { SERVICE_TYPE } from '../../../../../constants/serviceType';
-import { selectPageCreditReceived } from '../../../../../store/slices/nova/pageStatusSlice';
-import { selectTabSlice } from '../../../../../store/slices/tabSlice';
-import { useAppSelector } from '../../../../../store/store';
-import { getCookie } from '../../../../../util/common';
 import { useTranslationContext } from '../../provider/translation-provider';
 import TranslationResultAction from '../translation-result-action';
 
