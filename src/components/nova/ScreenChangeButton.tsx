f68a61d71927f667bc79c4e09a98a474b53e0bf3
luna <luna.s.jo@polarisoffice.com>
2025-03-21 16:32:03 +0900

    hotfix: [QA][NOVA] PC에서 NOVA 바텀 화면 확대/축소 기능 노출되지 않음 (SVR-5880)

--- src/components/nova/ScreenChangeButton.tsx
+++ src/components/nova/ScreenChangeButton.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from 'react';
 import { ReactComponent as MaxDarkIcon } from 'img/dark/nova/ico_max.svg';
 import { ReactComponent as MinDarkIcon } from 'img/dark/nova/ico_min.svg';
 import { ReactComponent as MaxLightIcon } from 'img/light/nova/ico_max.svg';
 import { ReactComponent as MinLightIcon } from 'img/light/nova/ico_min.svg';
 
+import usePostSplunkLog from '../../api/usePostSplunkLog';
+import { ClientStatusType } from '../../pages/Nova/Nova';
 import {
   selectPageCreditReceived,
   selectPageService
@@ -29,13 +32,22 @@ export const ScreenChangeButton = () => {
   const service = useAppSelector(selectPageService(selectedNovaTab));
   const showSurveyModal = UseShowSurveyModal();
 
-  console.log('from: ', from);
-  console.log('platform: ', platform);
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    Bridge.callSyncBridgeApiWithCallback({
+      api: 'getClientStatus',
+      callback: async (status: ClientStatusType) => {
+        setIsShow(status === 'home');
+      }
+    });
+  }, []);
+
   if (
     ((platform === ClientType.web ||
       platform === ClientType.windows ||
       platform === ClientType.mac) &&
-      from === 'home') ||
+      isShow) ||
     ((platform === ClientType.ios || platform === ClientType.android) &&
       device === DeviceType.phone)
   ) {
