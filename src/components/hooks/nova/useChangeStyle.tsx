bcb3e54a9f4385955fff31f79806f0d4c113b0f9
luna <luna.s.jo@polarisoffice.com>
2025-06-12 13:55:16 +0900

    fix: [QA][NOVA][Chrome/Safari] 스타일변환 동작시, 생성완료된 이미지 깨지는 현상 (#SVR-6658)

--- src/components/hooks/nova/useChangeStyle.tsx
+++ src/components/hooks/nova/useChangeStyle.tsx
@@ -90,13 +90,13 @@ export const useChangeStyle = () => {
 
       const response = await res.json();
       if (response.success) {
+        const image = response.data.image[0];
         dispatch(
           setPageResult({
             tab: NOVA_TAB_TYPE.changeStyle,
             result: {
-              contentType: '',
-              data: '',
-              link: response.data.imageUrl,
+              contentType: image.contentType,
+              data: image.data,
               info: style
             }
           })
